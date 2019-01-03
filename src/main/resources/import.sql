c0e90392e9e94c709558d0ad6f5834560654ee30
Programito <Programito@gmail.com>
2019-01-03 20:03:57 +0100

    paginator

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Juan', 'Gil', 'Juan_Gil@gmail.com', '2018-10-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alan', 'Shearer', 'Alan_Shearer@gmail.com', '2015-01-15');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jesus', 'Ferran', 'Jesus_Ferran@gmail.com', '2018-11-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('David', 'Perez', 'David.Perez@gmail.com', '2018-05-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Helena', 'Lopez', 'Helena_Lopez@gmail.com', '2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jessica', 'Gata', 'Jessica_Gata@gmail.com', '2018-11-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Michael', 'Kloss', 'Michael.Kloss@gmail.com', '2018-02-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pablo', 'Contreras', 'Pablo_Contreras@gmail.com', '2018-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Cristina', 'Gosling', 'Cristina_Santos@gmail.com', '2017-10-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Marta', 'Santos', 'Marta_G@gmail.com', '2017-10-21');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Eloy', 'Pascual', 'Eloy_Pascual@gmail.com', '2017-10-22');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Elvira', 'Rodriguez', 'Elvira_Rodriguez@gmail.com', '2017-10-17');
