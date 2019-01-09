822b6480ef0fe399106cf7654ffdfd94db58776c
Programito <Programito@gmail.com>
2019-01-09 19:28:22 +0100

    spring-security-oauth

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,8 +10,6 @@ INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
 INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida');
 
 
-
-
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4,'Juan', 'Gil', 'Juan_Gil@gmail.com', '2018-10-11');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1,'Alan', 'Shearer', 'Alan_Shearer@gmail.com', '2015-01-15');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(2,'Jesus', 'Ferran', 'Jesus_Ferran@gmail.com', '2018-11-07');
@@ -24,3 +22,14 @@ INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1,'C
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(5,'Marta', 'Santos', 'Marta_G@gmail.com', '2017-10-21');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(6,'Eloy', 'Pascual', 'Eloy_Pascual@gmail.com', '2017-10-22');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(7,'Elvira', 'Rodriguez', 'Elvira_Rodriguez@gmail.com', '2017-10-17');
+
+INSERT INTO `usuarios` (username, password,enabled) VALUES ('Alan','$2a$10$J1o3Cf6cOyFFeoEapDUwKuost5eDINHg0QrpNxWBmBzKw7UAWOO.2','1');
+INSERT INTO `usuarios` (username, password,enabled) VALUES ('admin','$2a$10$QbN6LhlOOm1AfACFG1ksvOVgMdVj4ph9fiwnaMlysWFzMnLiycK6q','1');
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(1,1);
+INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(2,2);
+INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(2,1);
+
